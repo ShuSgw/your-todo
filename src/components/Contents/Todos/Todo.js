1d32a2b1a427db5ee83aacef610a5ab9c120dce9
Shuhei Shagawa <sgw.sgw.shu@gmail.com>
2020-02-18 23:26:09 -0800

    [add] delete and edit view

--- src/components/Contents/Todos/Todo.js
+++ src/components/Contents/Todos/Todo.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { ListGroupItem } from "reactstrap";
+import { Row, Col, Button } from "reactstrap";
 
 const Todo = props => (
   <ListGroupItem className="justify-content-between">
-    {props.todo}
+    <Row>
+      <Col xs="7">{props.todo}</Col>
+      <Col xs="5">
+        <Button className="w-100 mb-1">Edit</Button>
+        <Button className="w-100">Delete</Button>
+      </Col>
+    </Row>
   </ListGroupItem>
 );
 
